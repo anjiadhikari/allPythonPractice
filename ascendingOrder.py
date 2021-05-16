c768d035f3a792c8568ba95b0fa7e7e59d5e53a1
Anjil Adhikari <anjiladlee10@gmail.com>
2021-05-16 07:53:45 +0545

    Update ascendingOrder.py

--- ascendingOrder.py
+++ ascendingOrder.py
@@ -19,3 +19,26 @@ print('\n All the numbers after in Ascending order :')
 numAll.sort()
 print(numAll)
 
+#using while loope for sorting in asceding order
+numbers =list(map(int,input('Enter the number to be sorted :').split()))
+
+sortedNum=[]
+i=0
+length=len(numbers)
+while(i < length):
+	tempo = min(numbers)
+	sortedNum.append(tempo)
+	numbers.remove(tempo)
+	i+=1
+
+print('All Numbers after sorting is::'  ,sortedNum)
+
+
+
+
+
+
+
+
+
+
